e24a4bd7501a3a6a6d4cdf2dee15d136259789e1
kensey1986 <55785690+kensey1986@users.noreply.github.com>
2021-05-06 22:50:42 -0500

    dsd

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   //URL: "http://localhost:80/",
-  URL:process.env.API || "http://localhost:8080/api/"
+  URL:process.env.API || "https://backenddomicilios.herokuapp.com/api/"
    //URL:"http://192.168.1.156:8080/"
 };
