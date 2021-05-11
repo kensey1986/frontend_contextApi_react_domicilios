daf1d83182864650125d3312f7402d87e123bd39
kensey1986 <55785690+kensey1986@users.noreply.github.com>
2021-05-10 22:45:50 -0500

    apirest

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   //URL: "http://localhost:80/",
-  URL:process.env.API || "https://backenddomicilios.herokuapp.com/api/"
+  URL:process.env.API || "http://181.48.242.146:4000/api/"
    //URL:"http://192.168.1.156:8080/"
 };
